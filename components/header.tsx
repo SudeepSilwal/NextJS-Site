558e3e68d9a5c665607b555e7e00dc86254be925
SudeepSilwal <officialsudeepsilwal@gmail.com>
2026-09-07 00:20:53 +0545

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
       <nav className='container flex max-w-3xl items-center justify-between'>
+        
         <div>
           <Link href='/' className='font-serif text-2xl font-bold'>
             Sudeep
@@ -12,15 +13,29 @@ export default function Header() {
         </div>
 
         <ul className='flex items-center gap-6 text-sm font-light text-muted-foreground sm:gap-10'>
+          
           <li className='transition-colors hover:text-foreground'>
-            <Link href='blog.sudeepsilwal.com.np'>Posts</Link>
+            <a
+              href='https://blog.sudeepsilwal.com.np/posts'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Posts ↗
+            </a>
           </li>
+
           <li className='transition-colors hover:text-foreground'>
-            <Link href='blog.sudeepsilwal.com.np'>Projects</Link>
+            <Link href='/projects'>
+              Projects
+            </Link>
           </li>
+
           <li className='transition-colors hover:text-foreground'>
-            <Link href='/contact'>Contact</Link>
+            <Link href='/contact'>
+              Contact
+            </Link>
           </li>
+
         </ul>
 
         <div>
